25e608b9a634f88086dc139a85e84ab1723b3dd5
brenimdobr <127851221+brenimdobr@users.noreply.github.com>
2024-07-01 11:00:38 -0300

    Add files via upload

---
+++ ia-gora-main/main.js
@@ -0,0 +1,14 @@
+const caixaPrincipal = document.querySelector(".caixa-principal")
+const caixaPerguntas = document.querySelector(".caixa-perguntas")
+const caixaAlternativas = document.querySelector(".caixa-alternativas")
+const caixaResultado = document.querySelector(".caixa-resultado")
+const textoResultado = document.querySelector(".texto-resultado")
+
+const caixaResultado1 = document.querySelector(".caixa-principal")
+
+const perguntas = [
+    {
+        enunciado:"Como a música e as flores podem ser usadas para promover a inclusão social em diferentes comunidades?",
+        alternativas:[]
+    }
+]
